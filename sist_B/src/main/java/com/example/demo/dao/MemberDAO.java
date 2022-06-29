60b267185e28240e4e2bb025136638db3a48f383
kikk789 <kakk789@naver.com>
2022-06-29 14:26:49 +0900

    게시물 추가 완성(이창희)

---
+++ sist_B/src/main/java/com/example/demo/dao/MemberDAO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+
+	public int insert() {
+		System.out.println("등록하였습니다.");
+		return 1;
+	}
+}
