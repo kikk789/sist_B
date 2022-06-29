e4db81832c60b9c4bc724e9511cd722bca37bb18
doyeon111 <lby9905@naver.com>
2022-06-29 14:32:54 +0900

    MemberDAO, listBoard.html [김도연]

---
+++ sist_B/src/main/java/com/example/demo/dao/MemberDAO.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	public void insert() {
+		System.out.println("OK");
+	}
+}
